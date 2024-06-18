53752e243ee6f426dbcebf40f876cbca39c6a28b
npaviliuc <n.paviliuc@studenti.uniba.it>
2024-06-18 11:06:35 +0200

    added some test code

--- src/test/java/RemoveSubStringTest.java
+++ src/test/java/RemoveSubStringTest.java
@@ -1,7 +1,35 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class RemoveSubStringTest {
 
+    private static RemoveSubString remove;
 
+    @BeforeAll
+    public static void init() {
+        remove = new RemoveSubString();
+    }
+
+    @Test
+    public void testStringNull() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> remove.removeSubString(null, "Ciao"));
+    }
+
+    @Test
+    public void testStringEmpty() {
+        Assertions.assertEquals("Substring not found!", remove.removeSubString("","Bella"));
+    }
+
+    @Test
+    public void testSubstringNull() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> remove.removeSubString("Come va?", null));
+    }
+
+    @Test
+    public void testSubstringEmpty() {
+        Assertions.assertEquals("Come andiamo?", remove.removeSubString("Come andiamo?", ""));
+    }
 }
